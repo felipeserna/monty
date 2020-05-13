6b1a4b415682336aa5c03ab12282561dd3b44037
laucavv <laucavv23@gmail.com>
2020-05-13 22:17:22 +0000

    change * a &

--- exe_line.c
+++ exe_line.c
@@ -10,22 +10,23 @@ void exe_line(void)
 	
 	char *tk = NULL;
 	unsigned int line_number = 0;
-	stack_t **head = NULL;
+	stack_t *head = NULL;
 	void (*select)(stack_t **stack, unsigned int line_number);
 	/*char *line = NULL;*/
 
 	printf("0");
 	while (read_f() != EOF)
-	{printf("1");
+	{printf("1\n");
 		line_number += 1;
 		tk = strtok(mont.buffer, " \t\n");
 		select = get_opcode(tk, line_number);
 		if (select)
-		{printf("2");
+		{printf("2\n");
 			mont.num = strtok(NULL, " \t\n");
-			printf("mont numn %s",mont.num);
-			//select(head, line_number);
+			printf("mont numn %s\n",mont.num);
+			select(&(head), line_number);
 		}
+		free(mont.buffer);
 		printf("\n");
 	}
 	close_f();
