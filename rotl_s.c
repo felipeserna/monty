60ad0fc9c46ea347a794f0892eed8533a2d7c113
laucavv <laucavv23@gmail.com>
2020-05-14 21:36:21 +0000

    rotl_s task 13

---
+++ rotl_s.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+ * rotl_s -  rotates the stack to the top.
+ * @stack: stack
+ * @line_number: line number
+ * Return: void
+ */
+void rotl_s(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = NULL, *tmp2 = NULL;
+	int num1 = 0;
+	(void)line_number;
+
+	tmp = *stack;
+	tmp2 = tmp->next;
+	num1 = tmp->n;
+
+	while (tmp->next)
+	{
+		tmp->n = tmp2->n;
+		tmp = tmp2;
+		tmp2 = tmp->next;
+	}
+	tmp->n = num1;
+}
