58420d725ce2d64635da4e61bbadacdfb20ce4c4
laucavv <laucavv23@gmail.com>
2020-05-14 01:49:27 +0000

    add \n

--- pint_s.c
+++ pint_s.c
@@ -13,7 +13,7 @@ void pint_s(stack_t **stack, unsigned int line_number)
 
 	if (tmp == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", tmp->n);
