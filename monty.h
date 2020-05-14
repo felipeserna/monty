09b76f8dd8d4e88453f5b518b201fe57e1f7f264
laucavv <laucavv23@gmail.com>
2020-05-14 21:38:06 +0000

    add rotl_s

--- monty.h
+++ monty.h
@@ -66,6 +66,7 @@ void mul_s(stack_t **stack, unsigned int ln);
 void mod_s(stack_t **stack, unsigned int ln);
 void pchar_s(stack_t **stack, unsigned int ln);
 void pstr_s(stack_t **stack, unsigned int ln);
+void rotl_s(stack_t **stack, unsigned int ln);
 int is_number();
 void open_f(char *file_name);
 int read_f(void);
