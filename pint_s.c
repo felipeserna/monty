6bff4cad6dce70732aacc5c11fcece3ff5c17ee7
laucavv <laucavv23@gmail.com>
2020-05-14 01:34:29 +0000

    pint

---
+++ pint_s.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * pint_s - prints the value at the top of the stack
+ * @stack: stack
+ * @line_number: line number
+ * Return: void
+ */
+void pint_s(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	tmp = *stack;
+
+	if (tmp == NULL)
+	{
+		fprintf(stderr, " L%d: can't pint, stack empty", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", tmp->n);
+}
